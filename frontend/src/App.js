994797be8315e2e4714621e1835f71e2f0909857
Karhunen <juho.karhunen@tuni.fi>
2023-04-04 13:03:19 +0300

    Add basic frontend for making orders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import API from './API.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tänne tulee joskus leipiä
-        </a>
-      </header>
-    </div>
+    <API></API>
   );
 }
 
