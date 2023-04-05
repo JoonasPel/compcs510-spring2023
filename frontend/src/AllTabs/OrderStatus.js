80d8b118f50b8b257613f9c03c5e99c8aa6d9c85
Karhunen <juho.karhunen@tuni.fi>
2023-04-06 02:18:41 +0300

    Making basic frontend

---
+++ frontend/src/AllTabs/OrderStatus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function OrderStatus(){
+    return (
+    <div>
+        <div>OrderStatus</div>
+
+    </div>
+    )
+}
+
+export default OrderStatus;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,35 @@
-
+import React, { useState } from 'react';
 import './App.css';
-import API from './API.js'
+import MakeOrder from './AllTabs/MakeOrder'
+import OrderStatus from './AllTabs/OrderStatus';
+import User from './AllTabs/User';
+
 
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('API');
+
+  const handleNavClick = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <API></API>
+    <div>
+      <nav className='flex'>
+        <button className="pill-button">
+          <span className="button-text" onClick={() => handleNavClick('API')}>Make Order</span>
+          <hr className="button-divider" />
+          <span className="button-text" onClick={() => handleNavClick('OrderStatus')}>Order Status</span>
+          <hr className="button-divider" />
+          <span className="button-text" onClick={() => handleNavClick('User')}>User</span>
+        </button>
+      </nav>
+      <div>
+        {currentPage === 'API' && <MakeOrder />}
+        {currentPage === 'OrderStatus' && <OrderStatus />}
+        {currentPage === 'User' && <User />}
+      </div>
+    </div>
   );
 }
 
