5c5145a2b9f11b20b0e3dc5bddcb297293971183
Karhunen <juho.karhunen@tuni.fi>
2023-04-18 17:33:07 +0300

    Made a basic login create user and login system

--- frontend/src/AllTabs/OrderStatus.js
+++ frontend/src/AllTabs/OrderStatus.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function OrderStatus(){
-    return (
+function OrderStatus() {
+  const [orderId, setOrderId] = useState('');
+  const [status, setStatus] = useState('');
+
+  const handleOrderIdChange = (event) => {
+    setOrderId(event.target.value);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3001/order/${orderId}`);
+      const data = await response.json();
+      setStatus(data.status);
+    } catch (error) {
+      console.error('Error fetching order status:', error);
+    }
+  };
+
+  return (
     <div>
-        <div>OrderStatus</div>
-
+      <div>Order Status: {status}</div>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="orderId">Order ID:</label>
+        <input
+          type="text"
+          id="orderId"
+          value={orderId}
+          onChange={handleOrderIdChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
-    )
+  );
 }
 
 export default OrderStatus;
