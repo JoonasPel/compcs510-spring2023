735458516784859b9dd8fb30b16317f9f5689b18
Juho Nuottimäki <juho.nuottimaki@tuni.fi>
2023-04-20 20:13:52 +0300

    add wip code
    this is to

--- backend/server_b/server_b.js
+++ backend/server_b/server_b.js
@@ -1,24 +1,45 @@
 const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://guest:guest@rabbitmq:5672', function(error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
+// rabbitmq configs
+const ORDER_QUEUE = "orderQueue";
+const STATUS_QUEUE = "statusQueue";
+const rabbitPORT = 5672;
+let rabbitChannel;
+// Nodejs server configs
+const app = express();
+const nodejsPORT = 8080;
+
+async function handleOrder() {
+    amqp.connect('amqp://guest:guest@rabbitmq:5672', (error0, connection) => {
+        if (error0) {
+            throw error0;
         }
-
-        const queue = "orderQueue";
-        channel.assertQueue(queue, { durable: false });
-
-        console.log("!!!!!!! I AM SERVER B WAITING FOR MESSAGES !!!!!!!!!!!");
-        channel.consume(queue, function(msg) {
-            console.log("Received message in server B: ", msg.content.toString());
-        }, {
-            // this means that consumer WILL NOT send confirmation to rabbit
-            // about receiving the msg. probably not good to use
-            noAck: true
+        connection.createChannel((error1, channel) => {
+            if (error1) {
+                throw error1;
+            }
+    
+            // This makes sure that both of the queues have been created
+            await channel.assertQueue(ORDER_QUEUE, { durable: ture });
+            await channel.assertQueue(STATUS_QUEUE, { durable: true });
+    
+            console.log("Order handler connected");
+    
+            channel.consume(ORDER_QUEUE, msg => {
+                console.log("Received order: ", msg.content.toString());
+                msg.content.parse
+                setTimeout( () => {
+                    const payload = msg.content.toString();
+                    const obj = JSON.parse(payload);
+                    obj.status = 'Ready';
+                    channel.sendToQueue()
+                }, 7000)
+    
+            }, {
+                // this means that consumer WILL NOT send confirmation to rabbit
+                // about receiving the msg. probably not good to use
+                noAck: false
+            });
         });
-    });
-});
+    });    
+}
