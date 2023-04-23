f3bf2bda42254acba60d7ad46486d908386ca6f4
Karhunen <juho.karhunen@tuni.fi>
2023-04-24 01:47:18 +0300

    Added some gifs for the app to be more usable by users

--- frontend/src/AllTabs/OrderStatus.js
+++ frontend/src/AllTabs/OrderStatus.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './OrderStatus.css';
+import logo from '../Images/food.gif';
+import delivery from '../Images/delivery.gif';
 
 function OrderStatus(props) {
   const orderedSandwiches = props.orderedSandwiches;
   const [statusArray, setStatusArray] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getOrderStatus = async (orderedSandwiches) => {
     const fetchedStatusArray = [];
@@ -21,17 +24,39 @@ function OrderStatus(props) {
     const intervalId = setInterval(async () => {
       const fetchedStatusArray = await getOrderStatus(orderedSandwiches);
       setStatusArray(fetchedStatusArray);
-    }, 3000);
+      setIsLoading(false); // Update isLoading state variable
+    }, 1000);
 
     return () => clearInterval(intervalId);
   }, [orderedSandwiches]);
 
+  const allReady = statusArray.every((item) => item.status === 'ready');
+
   return (
     <div className="order-status-container">
-      <label> Page refreshes automatically, please wait. </label>
-      <ul className="order-status-list">
+      {isLoading && orderedSandwiches.length > 0 && (
+        <label>Please wait orders will refresh automatically.</label>
+      )}
+      {!isLoading && allReady && orderedSandwiches.length > 0 && (
+        <>
+          <label>Your order is ready for delivery.</label>
+          <img src={delivery} height={100} width={150} />
+        </>
+      )}
+      {!isLoading && orderedSandwiches.length > 0 && !allReady && (
+        <>
+          <label>Food is being prepared.</label>
+          <img src={logo} height={100} width={100} />
+        </>
+      )}
+      {orderedSandwiches.length === 0 && (
+        <label>You have no active orders.</label>
+      )}
+      <ul className={`order-status-list ${isLoading ? 'hidden' : ''}`}>
         {statusArray.sort((a, b) => b.id - a.id).map((item) => (
-          <div key={item.id}>Order ID: {item.id}, Order Status: {item.status}</div>
+          <div key={item.id}>
+            Order ID: {item.id}, Order Status: {item.status}
+          </div>
         ))}
       </ul>
     </div>
